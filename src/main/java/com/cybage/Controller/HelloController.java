27802151e65c1706873bcd63c1c26813e410dd4f
sushma <sushmasarode1998@gmail.com>
2023-09-18 17:00:38 +0530

    first commit for cicd

---
+++ src/main/java/com/cybage/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.cybage.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/welcome")
+	public String getMessage()
+	{
+		return "Welcome to tech soft!!";
+	}
+	
+
+}
